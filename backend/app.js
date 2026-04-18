e4e1a2b62a3f3f80b1d7036fedc383526a5074f1
Pugazhenthi A <pugazhanbu1999@gmail.com>
2026-04-18 16:26:13 +0530

    JWT authentication done

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/.env" });
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 //import
 const jobRoutes = require("./routes/jobRoutes.js");
 const auth = require("./routes/auth.js");
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1", jobRoutes);
 app.use("/api/v1", auth);
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,18 +4,17 @@ const User = require("../models/User");
 const ErrorHandler = require("../utils/errorHandler");
 
 const isAuthenticate = asyncHandler(async (req, res, next) => {
-  const { token } = req.cookies;
-
+  const token = req.cookies?.token;
+  // const authHeader = req.headers.authorization;
   if (!token) {
+    // if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return next(new ErrorHandler("Login user first to access", 401));
   }
-
-  const decoded = jwt.verify(token, process.env.JWT_SECERT);
+  // const token = authHeader.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
   //for more security or production we have to fetch from db. why bcoz if user role admin to user change. still that user as admin if u use jwt token payload. bcoz token expire 1d. so better use we have to hit DB on every request.
 
   // req.user = await User.findById(decoded.id);
-
-  //for small and mid apps we can use it for token
   req.user = decoded;
   next();
 });
